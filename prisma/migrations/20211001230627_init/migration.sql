a3bdd49c25bc2a51a504647de06234598e1b5ffd
Eduardolovo <eduardo.llovo@gmail.com>
2021-10-01 20:18:17 -0300

    migration

--- prisma/migrations/20211001230627_init/migration.sql
+++ prisma/migrations/20211001230627_init/migration.sql
@@ -39,29 +39,42 @@ CREATE TABLE `Jogo` (
 -- CreateTable
 CREATE TABLE `Genero` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
-    `jogoId` INTEGER NOT NULL,
     `nome` VARCHAR(191) NOT NULL,
 
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- CreateTable
-CREATE TABLE `__MyRelationTable` (
-    `A` INTEGER NOT NULL,
-    `B` INTEGER NOT NULL,
+CREATE TABLE `JogosOnGeneros` (
+    `jogoId` INTEGER NOT NULL,
+    `generoId` INTEGER NOT NULL,
+    `assignedAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `assignedBy` VARCHAR(191) NOT NULL,
 
-    UNIQUE INDEX `__MyRelationTable_AB_unique`(`A`, `B`),
-    INDEX `__MyRelationTable_B_index`(`B`)
+    PRIMARY KEY (`jogoId`, `generoId`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `PerfisOnJogos` (
+    `perfilId` INTEGER NOT NULL,
+    `jogoId` INTEGER NOT NULL,
+    `assignedAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `assignedBy` VARCHAR(191) NOT NULL,
+
+    PRIMARY KEY (`perfilId`, `jogoId`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- AddForeignKey
 ALTER TABLE `Perfil` ADD CONSTRAINT `Perfil_usuarioId_fkey` FOREIGN KEY (`usuarioId`) REFERENCES `Usuario`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE `Genero` ADD CONSTRAINT `Genero_jogoId_fkey` FOREIGN KEY (`jogoId`) REFERENCES `Jogo`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE `JogosOnGeneros` ADD CONSTRAINT `JogosOnGeneros_jogoId_fkey` FOREIGN KEY (`jogoId`) REFERENCES `Jogo`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE `__MyRelationTable` ADD FOREIGN KEY (`A`) REFERENCES `Jogo`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE `JogosOnGeneros` ADD CONSTRAINT `JogosOnGeneros_generoId_fkey` FOREIGN KEY (`generoId`) REFERENCES `Genero`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE `__MyRelationTable` ADD FOREIGN KEY (`B`) REFERENCES `Usuario`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE `PerfisOnJogos` ADD CONSTRAINT `PerfisOnJogos_perfilId_fkey` FOREIGN KEY (`perfilId`) REFERENCES `Perfil`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `PerfisOnJogos` ADD CONSTRAINT `PerfisOnJogos_jogoId_fkey` FOREIGN KEY (`jogoId`) REFERENCES `Jogo`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
